9e77679e6769c28316fcf53a8ec59a0bd903a2d5
JordanCeres <62607050+Supaishichikin@users.noreply.github.com>
2022-12-21 19:52:26 +0100

    fix login
    fix login

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from app.routers.dependencies.userManager import fastapi_users
 from app.schemas.user import UserRead, UserUpdate, UserCreate
+from app.routers.dependencies.userManager import auth_backend
 
 router = APIRouter()
 
+router.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/jwt", tags=["auth"])
 router.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), tags=["users"])
 router.include_router(fastapi_users.get_register_router(UserRead, UserCreate), tags=["auth"])
 router.include_router(fastapi_users.get_verify_router(UserRead), tags=["auth"])
